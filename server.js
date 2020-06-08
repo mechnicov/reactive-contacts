b0b32e3480eb52b169a2183eb474ee563a12b3da
Edem Topuzov <keltikan@gmail.com>
2020-06-09 00:49:02 +0300

    Add basic routes for API

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) =>
   res.json({msg: 'Welcome to contacts keeper API'})
 )
 
+app.use('/api/v1/users', require('./routes/api/v1/users'))
+app.use('/api/v1/auth', require('./routes/api/v1/auth'))
+app.use('/api/v1/contacts', require('./routes/api/v1/contacts'))
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
