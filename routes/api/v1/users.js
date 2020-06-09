a241b11c40e4fa8fb3ea65a735f7ce59b04d5443
Edem Topuzov <keltikan@gmail.com>
2020-06-09 16:15:41 +0300

    Get token if user registration is ok

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const { check, validationResult } = require('express-validator/check')
 
 const User = require('../../../models/User')
@@ -17,6 +19,7 @@ router.post(
   ],
   async (req, res) => {
     const errors = validationResult(req)
+
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() })
     }
@@ -38,7 +41,21 @@ router.post(
 
       await user.save()
 
-      res.send('User saved')
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err
+          res.json({ token })
+        }
+      )
     } catch (err) {
       console.error(err.message)
       res.status(500).send('Server Error')
