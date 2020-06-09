113b34a34b3b3a98e66cb620bce6e1406970b420
Edem Topuzov <keltikan@gmail.com>
2020-06-09 12:12:37 +0300

    Validate post requests for user register

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator/check')
+
+const User = require('../../../models/User')
 
 // @route   POST  api/v1/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Register a user')
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Email is invalid').isEmail(),
+    check('password', 'Password must contains minimun 6 characters').isLength({ min: 6 })
+  ],
+  (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() })
+  }
+
+  res.send('passed')
 })
 
 module.exports = router
