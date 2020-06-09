ded2651aae6010e9c06d956360c46e3869eabe10
Edem Topuzov <keltikan@gmail.com>
2020-06-09 11:39:00 +0300

    Use express middleware for incoming JSON parse

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const connectDB = require('./config/db')
 const app = express()
 connectDB()
 
+app.use(express.json({ extended: false }))
+
 app.get('/', (req, res) =>
   res.json({msg: 'Welcome to contacts keeper API'})
 )
