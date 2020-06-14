856da34fe54897a3bebb0a0bb53ebab4530a24d1
Edem Topuzov <keltikan@gmail.com>
2020-06-14 23:40:32 +0300

    Implement add contact route

--- routes/api/v1/contacts.js
+++ routes/api/v1/contacts.js
@@ -22,9 +22,33 @@ router.get('/', auth, async (req, res) => {
 // @route   POST  api/v1/contacts
 // @desc    Add new contact
 // @access  Private
-router.post('/', (req, res) => {
-  res.send('Add new contact')
-})
+router.post(
+  '/',
+  [
+    auth,
+    check('name', 'Name is required').not().isEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() })
+    }
+
+    const { name, email, phone, type } = req.body
+
+    try {
+      const newContact = new Contact({ name, email, phone, type, user: req.user.id })
+
+      const contact = await newContact.save()
+
+      res.json(contact)
+    } catch (err) {
+      console.error(err.message)
+      res.status(500).send('Server Error')
+    }
+  }
+)
 
 // @route   PUT  api/v1/contacts/:id
 // @desc    Update contact
