57a949672ac20bca0dcf6b629fe0a8d10b665ea9
Edem Topuzov <keltikan@gmail.com>
2020-06-16 18:30:34 +0300

    Add navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import Navbar from './components/layout/Navbar'
+import './App.css'
 
 const App = () => {
   return (
     <div className="App">
+      <Navbar/>
       Contacts Keeper
     </div>
   );
