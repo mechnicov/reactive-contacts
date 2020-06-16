f7ca3e22b736554765082695e9f82b1f5e934765
Edem Topuzov <keltikan@gmail.com>
2020-06-16 23:49:18 +0300

    Contact item UI

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -9,10 +9,21 @@ const ContactItem = ({ contact }) => {
       <h3 className='text-primary text-left'>
         {name}
         {' '}
-        <span className={`badge ${type === 'professional' ? 'badge-success' : 'badge-primary'}`}>
+        <span
+          className={`badge ${type === 'professional' ? 'badge-success' : 'badge-primary'}`}
+          style={{ float: 'right' }}
+        >
           {capitalize(type)}
         </span>
       </h3>
+      <ul className='list'>
+        {email && <li><i className='fas fa-envelope-open'/> {email}</li>}
+        {phone && <li><i className='fas fa-phone'/> {phone}</li>}
+      </ul>
+      <p>
+        <button className='btn btn-dark btn-sm'>Edit</button>
+        <button className='btn btn-danger btn-sm'>Delete</button>
+      </p>
     </div>
   )
 }
