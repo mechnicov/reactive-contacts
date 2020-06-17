a6610c99e7e641d90f5f7ec5be27c6754530c991
Edem Topuzov <keltikan@gmail.com>
2020-06-17 21:42:47 +0300

    Realize adding contact in contact state

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react'
-import uuid from 'uuid'
+import { v4 as uuid } from 'uuid'
 import ContactContext from './contactContext'
 import ContactReducer from './contactReducer'
 import {
@@ -35,6 +35,10 @@ const ContactState = props => {
   const [state, dispatch] = useReducer(ContactReducer, initialState)
 
   // Add contact
+  const addContact = contact => {
+    contact.id = uuid()
+    dispatch({ type: ADD_CONTACT, payload: contact })
+  }
 
   // Delete contact
 
@@ -51,7 +55,8 @@ const ContactState = props => {
   return (
     <ContactContext.Provider
       value={{
-        contacts: state.contacts
+        contacts: state.contacts,
+        addContact
       }}
     >
       {props.children}
