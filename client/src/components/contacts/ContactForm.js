ad4119628a066b8b27e168b2201b93d6702a7179
Edem Topuzov <keltikan@gmail.com>
2020-06-17 21:43:13 +0300

    Adding contact using contact form

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import ContactContext from '../../context/contact/contactContext'
 
 const ContactForm = () => {
+  const contactContext = useContext(ContactContext)
+
   const [contact, setContact] = useState({
     name: '',
     email: '',
@@ -12,8 +15,19 @@ const ContactForm = () => {
 
   const onChange = e => setContact({ ...contact, [e.target.name]: e.target.value })
 
+  const onSubmit = e => {
+    e.preventDefault()
+    contactContext.addContact(contact)
+    setContact({
+      name: '',
+      email: '',
+      phone: '',
+      type: 'personal'
+    })
+  }
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h2 className='text-primary'>Add Contact</h2>
       <input
         type='text'
@@ -21,6 +35,7 @@ const ContactForm = () => {
         name='name'
         value={name}
         onChange={onChange}
+        required
       />
       <input
         type='email'
@@ -28,6 +43,7 @@ const ContactForm = () => {
         name='email'
         value={email}
         onChange={onChange}
+        required
       />
       <input
         type='text'
@@ -37,8 +53,20 @@ const ContactForm = () => {
         onChange={onChange}
       />
       <h5>Contact Type</h5>
-      <input type='radio' name='type' value='personal' checked={type === 'personal'}/> Personal{' '}
-      <input type='radio' name='type' value='professional' checked={type === 'professional'}/> Professional{' '}
+      <input
+        type='radio'
+        name='type'
+        value='personal'
+        checked={type === 'personal'}
+        onChange={onChange}
+      /> Personal{' '}
+      <input
+        type='radio'
+        name='type'
+        value='professional'
+        checked={type === 'professional'}
+        onChange={onChange}
+      /> Professional{' '}
       <div>
         <input type='submit' value='Add Contact' className='btn btn-primary btn-block'/>
       </div>
