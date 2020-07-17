072076f9be18b9b35658e97237fe341942190fd0
Edem Topuzov <keltikan@gmail.com>
2020-07-17 16:08:31 +0300

    Add transition animations

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,4 +1,6 @@
 import React, { Fragment, useContext } from 'react'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+
 import ContactContext from '../../context/contact/contactContext'
 import ContactItem from './ContactItem'
 
@@ -12,9 +14,11 @@ const Contacts = () => {
 
   return (
     <Fragment>
-      {filtered === null ?
-        contacts.map(contact => <ContactItem key={contact.id} contact={contact}/>) :
-        filtered.map(contact => <ContactItem key={contact.id} contact={contact}/>)}
+      <TransitionGroup>
+        {filtered === null ?
+          contacts.map(contact => <CSSTransition key={contact.id} timeout={500} classNames='item'><ContactItem contact={contact}/></CSSTransition>) :
+          filtered.map(contact => <CSSTransition key={contact.id} timeout={500} classNames='item'><ContactItem contact={contact}/></CSSTransition>)}
+      </TransitionGroup>
     </Fragment>
   )
 }
