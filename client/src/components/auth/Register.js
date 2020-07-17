b75f3263995b9985645f5c698ced50f2cd233ad3
Edem Topuzov <keltikan@gmail.com>
2020-07-18 00:42:54 +0300

    Alert for registration form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import AlertContext from '../../context/alert/alertContext'
 
 const Register = () => {
+  const alertContext = useContext(AlertContext)
+  const { setAlert } = alertContext
+
   const [user, setUser] = useState({
     name: '',
     email: '',
@@ -14,7 +18,13 @@ const Register = () => {
 
   const onSubmit = e => {
     e.preventDefault()
-    console.log('Register')
+    if (name === '' || email === '' || password === '') {
+      setAlert('Please enter all data', 'danger')
+    } else if (password !== password2) {
+      setAlert('Passwords do not match', 'danger')
+    } else {
+      console.log('Register')
+    }
   }
 
   return (
@@ -25,19 +35,48 @@ const Register = () => {
       <form onSubmit={onSubmit}>
         <div className='form-group'>
           <label htmlFor='name'>Name</label>
-          <input type='text' name='name' id='name' value={name} onChange={onChange}/>
+          <input
+            type='text'
+            name='name'
+            id='name'
+            value={name}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className='form-group'>
           <label htmlFor='email'>Email</label>
-          <input type='email' name='email' id='email' value={email} onChange={onChange}/>
+          <input
+            type='email'
+            name='email'
+            id='email'
+            value={email}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className='form-group'>
           <label htmlFor='password'>Password</label>
-          <input type='password' name='password' id='password' value={password} onChange={onChange}/>
+          <input
+            type='password'
+            name='password'
+            id='password'
+            value={password}
+            onChange={onChange}
+            required
+            minLength='6'
+          />
         </div>
         <div className='form-group'>
           <label htmlFor='password2'>Confirm Password</label>
-          <input type='password' name='password2' id='password2' value={password2} onChange={onChange}/>
+          <input
+            type='password'
+            name='password2'
+            id='password2'
+            value={password2}
+            onChange={onChange}
+            required
+          />
         </div>
         <input type='submit' value='Register' className='btn btn-primary btn-block'/>
       </form>
--- client/src/components/layout/Alerts.js
+++ client/src/components/layout/Alerts.js
@@ -8,7 +8,7 @@ const Alerts = () => {
   return (
     alerts.length > 0 && alerts.map(alert => (
       <div key={alert.id} className={`alert alert-${alert.type}`}>
-        <i className='fas fa-onfo-circle'/> {alert.msg}
+        <i className='fas fa-info-circle'/> {alert.msg}
       </div>
     ))
   )
