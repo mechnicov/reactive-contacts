f24e80036888005d7064174d3c86d6d015557a46
Edem Topuzov <keltikan@gmail.com>
2020-07-18 00:20:00 +0300

    Remove alert after 5 seconds

--- client/src/context/alert/AlertState.js
+++ client/src/context/alert/AlertState.js
@@ -14,9 +14,9 @@ const AlertState = props => {
   const setAlert = (msg, type, timeout = 5000) => {
     const id = uuid()
     dispatch({type: SET_ALERT, payload: { msg, type, id }})
-  }
 
-  // Remove Alert
+    setTimeout(() => dispatch({type: REMOVE_ALERT, payload: id}), timeout)
+  }
 
   return (
     <AlertContext.Provider
