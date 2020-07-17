278635e1243b3d0fdb81a6604180d807e619f2f1
Edem Topuzov <keltikan@gmail.com>
2020-07-17 23:00:20 +0300

    Add Login component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -12,6 +12,7 @@ const Navbar = ({ title, icon }) => {
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/about'>About</Link></li>
         <li><Link to='/register'>Register</Link></li>
+        <li><Link to='/login'>Login</Link></li>
       </ul>
     </div>
   )
