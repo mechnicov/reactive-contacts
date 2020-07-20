549752f77b95743ca374ed6c8e37f5ef4e7a2acf
Edem Topuzov <keltikan@gmail.com>
2020-07-20 18:23:13 +0300

    Show validation error when some error during registration occured

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import AlertContext from '../../context/alert/alertContext'
 import AuthContext from '../../context/auth/authContext'
 
@@ -7,7 +7,14 @@ const Register = () => {
   const { setAlert } = alertContext
 
   const authContext = useContext(AuthContext)
-  const { register } = authContext
+  const { register, clearErrors, error } = authContext
+
+  useEffect(() => {
+    if (error) {
+      setAlert(error, 'danger')
+      clearErrors()
+    }
+  }, [error])
 
   const [user, setUser] = useState({
     name: '',
