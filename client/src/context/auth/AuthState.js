46da04883ba068f530be6298609bb60383ae2854
Edem Topuzov <keltikan@gmail.com>
2020-07-20 21:16:39 +0300

    User login implemented

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -58,6 +58,21 @@ const AuthState = props => {
   }
 
   // Login User
+  const login = async formData => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    try {
+      const res = await axios.post('/api/v1/auth', formData, config)
+      dispatch({type: LOGIN_SUCCESS, payload: res.data})
+      loadUser()
+    } catch (err) {
+      dispatch({type: LOGIN_FAIL, payload: err.response.data.msg})
+    }
+  }
 
   // Logout
 
@@ -73,6 +88,7 @@ const AuthState = props => {
         user: state.user,
         error: state.error,
         register,
+        login,
         loadUser,
         clearErrors,
       }}
